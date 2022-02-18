b5e49a0db31003d99d8af9af03ba09617488a09b
Willem Houck <willem.houck@hotmail.com>
2022-02-18 11:03:46 +0100

    fbeta loss

--- code/main.py
+++ code/main.py
@@ -52,7 +52,7 @@ def build_model(checkpoint_path, model_name='CloudXNet', lr=1e-4, size=256, resu
 
     # print(model.summary())
     
-    model.compile(optimizer = Adam(lr = lr), loss = jacc_loss, metrics = [dice_score,'accuracy'])
+    model.compile(optimizer = Adam(lr = lr), loss = fbeta_loss, metrics = [jacc_score,'accuracy'])
 
     if resume:
         print('Resuming from weights')
@@ -70,9 +70,7 @@ def train_model(model, train_im, train_an, val_im, val_an, pre_process, log_dir,
 
     utils.ensure_directory_existance(log_dir)
     log_name = "tensorboard"
-    tensorboard = TensorBoard(log_dir=os.path.join(log_dir, log_name))
-    checkpoint_path = os.path.join(log_dir, "weights.{epoch:02d}.hdf5")
-    checkpointer = ModelCheckpoint(checkpoint_path, monitor= "val_loss", save_best_only = True, save_weights_only=True)#, save_freq='epoch')
+
 
     if model_name == 'UNet':
         pre_run_epochs = 2
@@ -80,18 +78,20 @@ def train_model(model, train_im, train_an, val_im, val_an, pre_process, log_dir,
 
         # First run
         print(model.summary())
-        tensorboard = TensorBoard(log_dir=os.path.join(log_dir, log_name, 'run_1'))
+        utils.ensure_directory_existance(os.path.join(log_dir, 'run_1'))
+        tensorboard = TensorBoard(log_dir=os.path.join(log_dir, 'run_1', log_name))
         checkpoint_path = os.path.join(log_dir, 'run_1', "weights.{epoch:02d}.hdf5")
         checkpointer = ModelCheckpoint(checkpoint_path, monitor= "val_loss", save_weights_only=True, save_freq='epoch')
         results_1 = model.fit(train_gen, validation_data=val_gen, epochs=pre_run_epochs, verbose=1, callbacks=[tensorboard, checkpointer])
 
         # Second run
         print('Building second model without frozen weights')
-        weights_path = os.path.join(log_dir, 'run_1', "weights.{}.hdf5".format(pre_run_epochs))
+        utils.ensure_directory_existance(os.path.join(log_dir, 'run_2'))
+        weights_path = os.path.join(log_dir, 'run_1', "weights.0{}.hdf5".format(pre_run_epochs))
         model_2, pre_process = build_model(weights_path, model_name=model_name, size=size, resume=True, freeze_backbone=False)
         print(model_2.summary())
 
-        tensorboard = TensorBoard(log_dir=os.path.join(log_dir, log_name, 'run_2'))
+        tensorboard = TensorBoard(log_dir=os.path.join(log_dir, 'run_2', log_name))
         checkpoint_path = os.path.join(log_dir, 'run_2', "weights.{epoch:02d}.hdf5")
         checkpointer = ModelCheckpoint(checkpoint_path, monitor= "val_loss", save_best_only = True, save_weights_only=True)
 
@@ -100,6 +100,10 @@ def train_model(model, train_im, train_an, val_im, val_an, pre_process, log_dir,
 
 
     else:
+        tensorboard = TensorBoard(log_dir=os.path.join(log_dir, log_name))
+        checkpoint_path = os.path.join(log_dir, "weights.{epoch:02d}.hdf5")
+        checkpointer = ModelCheckpoint(checkpoint_path, monitor= "val_loss", save_best_only = True, save_weights_only=True)#, save_freq='epoch')
+
         results = model.fit(train_gen, validation_data=val_gen, epochs=epochs, verbose=1, callbacks=[tensorboard, checkpointer])
 
     # Write metrics
@@ -128,15 +132,15 @@ def create_tiles_for_prediction(tif_path, resize_factor=100, size=256, nr_channe
     blue = np.pad(downsized_img[2], pad_width = ((0,pad_height), (0,pad_width)))
 
     full_img = np.stack((red, green, blue), axis=2)
-    preprocessed_img = pre_process_max(full_img)
+    # preprocessed_img = pre_process(full_img)
     
     # loop over image and create size*size tiles with padding
     k=0
     tile_name = []
-    all_tiles = np.zeros((nr_tiles_h*nr_tiles_w, size , size, nr_channels), dtype=np.float32)
+    all_tiles = np.zeros((nr_tiles_h*nr_tiles_w, size , size, nr_channels), dtype=np.int64)
     for i in range(nr_tiles_h):
         for j in range(nr_tiles_w): #might need to swap nr_tiles_h/w
-            all_tiles[k,:,:,:] = preprocessed_img[i*size:(i+1)*size,j*size:(j+1)*size,:]#/255
+            all_tiles[k,:,:,:] = full_img[i*size:(i+1)*size,j*size:(j+1)*size,:]#/255
             tile_name.append(str(i*size) + '_' + str(j*size))
             k +=1
     
@@ -164,27 +168,27 @@ def load_test_images(test_im, test_an, size=256):
     return X_test, Y_test, image_name
     
 
-def predict(X_test, log_dir, filename, pre_process, Y_test=None, tile_name=None):
-    # pre_processed_X = np.zeros(X_test.shape, dtype=np.float32)
+def predict(X_test, pre_process, log_dir, filename, Y_test=None, tile_name=None):
+    pre_processed_X = np.zeros(X_test.shape, dtype=np.float32)
 
-    # for i in range(X_test.shape[0]):
-    #     pre_processed_X[i,:,:,:] = pre_process(X_test[i,:,:,:])
+    for i in range(X_test.shape[0]):
+        pre_processed_X[i,:,:,:] = pre_process(X_test[i,:,:,:])
 
-    preds_test = model.predict(X_test, batch_size=2, verbose=1) # X_test between 0 and 1
-    test_pred = (preds_test > 0.5).astype(int)
+    preds_test = model.predict(pre_processed_X, batch_size=2, verbose=1) # X_test between 0 and 1
+    test_pred = (preds_test > 0.0001).astype(int)
 
     for i in range(X_test.shape[0]):
         predictions = np.repeat(test_pred[i,:,:], 3, axis=2)
         
         if isinstance(Y_test, np.ndarray): 
             annotations = np.repeat(Y_test[i][...,np.newaxis], 3, axis=2)
-            concat_array = np.concatenate((X_test[i], annotations, predictions), axis=1)
+            concat_array = np.concatenate((X_test[i], annotations*255, predictions*255), axis=1)
         else:
-            concat_array = np.concatenate((X_test[i], predictions), axis=1)
+            concat_array = np.concatenate((X_test[i], predictions*255), axis=1)
 
-        concat_img = Image.fromarray((concat_array*255).astype(np.uint8), 'RGB')
+        concat_img = Image.fromarray(concat_array.astype(np.uint8), 'RGB')
 
-        save_dir = os.path.join(log_dir, filename + '_0.5_filt')
+        save_dir = os.path.join(log_dir, filename + '_0.0001')
         utils.ensure_directory_existance(save_dir)
 
         concat_img.save(os.path.join(save_dir, tile_name[i]+'.png'))
@@ -196,22 +200,20 @@ if __name__ == "__main__":
     print("Initializing")
 
     # dataset parameters
-    dataset = '/Users/Willem/Werk/510/510_cloud_detection/biome_input/' # name of folder 
-    path_data = '/' + dataset #/Users/Willem/Werk/510, /home/NC6user/510_cloud_detection/log/0209v0_biome_20ep
+    dataset = 'biome_input/' # name of folder 
+    path_data = '/Users/Willem/Werk/510/510_cloud_detection/' + dataset #/Users/Willem/Werk/510, /home/NC6user/510_cloud_detection/log/0209v0_biome_20ep
 
-    log_name = '0209v0_biome_20ep' # month, day, version, _model
+    log_name = '0215v0_biome_30ep' # month, day, version, _model
     log_dir = '/Users/Willem/Werk/510/510_cloud_detection/log/' + log_name  #save weights, results & tensorboard /home/NC6user/510_cloud_detection/log/
-    checkpoint_path = '/Users/Willem/Werk/510/510_cloud_detection/log/0209v0_biome_20ep/weights.08.hdf5' #'/Users/Willem/Werk/510/510_cloud_detection/log/0124v0_dataset_38_10ep/weights.08.hdf5' #'/Users/Willem/Werk/510/510_cloud_detection/log/0119v0_dataset_50ep/0119v0_dataset_50ep.h5'
-    maxar_path = '/Users/Willem/Werk/510/510_cloud_detection/geotiff_examples/maxar_clouds_small.tif'
-    # maxar_path = '/Users/Willem/Werk/510/510_cloud_detection/geotiff_examples/maxar_clouds.tif'
+    checkpoint_path = '/Users/Willem/Werk/510/510_cloud_detection/log/0215v0_biome_30ep/run_2/weights.21.hdf5' #'/Users/Willem/Werk/510/510_cloud_detection/log/0124v0_dataset_38_10ep/weights.08.hdf5' #'/Users/Willem/Werk/510/510_cloud_detection/log/0119v0_dataset_50ep/0119v0_dataset_50ep.h5'
+    maxar_path = '/Users/Willem/Werk/510/510_cloud_detection/geotiff_examples/maxar_white_buildings_1.tif'
 
-
-    train = True
-    resume = False
+    train = False
+    resume = True
     test_model = False
-    test_maxar = False 
+    test_maxar = True 
     model_name = 'UNet' #'CloudXNet', 'UNet'
-    epoch=20
+    epoch=30
     batch_size=4
     size=256
     lr=1e-4
@@ -247,8 +249,9 @@ if __name__ == "__main__":
 
     if test_maxar:
         print("Testing Maxar file")
+        # pre_process = pre_process_max #in case of testing CloudXNet model (preprocess train: /255, preprocess test: /max)
         downsized_img, X_test, tile_name = create_tiles_for_prediction(maxar_path, resize_factor=resize_factor, size=size, nr_channels=3) #resize factor depends on sensor resolution, landsat 30m, sentinel 10m, maxar 30cm & dataset resized 512->256 & 384 ->256
-        predict(X_test, tile_name=tile_name, log_dir=log_dir, pre_process=pre_process, filename=os.path.splitext(os.path.basename(maxar_path))[0])
+        predict(X_test, pre_process=pre_process, tile_name=tile_name, log_dir=log_dir, filename=os.path.splitext(os.path.basename(maxar_path))[0])
 
 
     
--- code/metrics.py
+++ code/metrics.py
@@ -7,17 +7,43 @@ from tensorflow.keras import backend as K
 smooth = 0.0000001
 
 
-def jacc_loss(y_true, y_pred):
+def jacc_score(y_true, y_pred):
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
     intersection = K.sum(y_true_f * y_pred_f)
-    return 1 - ((intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) - intersection + smooth))
+    jacc = (intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) - intersection + smooth)
+    return jacc
 
 
-def dice_score(y_true, y_pred, smooth=0.01):
-    y_true_f = K.flatten(y_true)
-    y_pred_f = K.flatten(y_pred)
-    intersection = K.sum(y_true_f * y_pred_f)
-    union = K.sum(y_true_f) + K.sum(y_pred_f)
-    dice = (2. * intersection + smooth)/(union + smooth)
-    return dice
+# def dice_score(y_true, y_pred):
+#     y_true_f = K.flatten(y_true)
+#     y_pred_f = K.flatten(y_pred)
+#     intersection = K.sum(y_true_f * y_pred_f)
+#     union = K.sum(y_true_f) + K.sum(y_pred_f)
+#     dice = (2. * intersection + smooth)/(union + smooth)
+#     return dice
+
+
+def fbeta_loss(ytrue , ypred, beta=2, threshold=0.5, epsilon=K.epsilon()):
+    # epsilon is set so as to avoid division by zero error
+    
+    beta_squared = beta**2 # squaring beta
+
+    # casting ytrue and ypred as float dtype
+    ytrue = tf.cast(ytrue, tf.float32)
+    ypred = tf.cast(ypred, tf.float32)
+
+    # setting values of ypred greater than the set threshold to 1 while those lesser to 0
+    ypred = tf.cast(tf.greater_equal(ypred, tf.constant(threshold)), tf.float32)
+
+    tp = tf.reduce_sum(ytrue*ypred) # calculating true positives
+    predicted_positive = tf.reduce_sum(ypred) # calculating predicted positives
+    actual_positive = tf.reduce_sum(ytrue) # calculating actual positives
+    
+    precision = tp/(predicted_positive+epsilon) # calculating precision
+    recall = tp/(actual_positive+epsilon) # calculating recall
+    
+    # calculating fbeta
+    fb = (1+beta_squared)*precision*recall / (beta_squared*precision + recall + epsilon)
+
+    return 1 - fb
